284b7f41f6825efd8973de9f4a01cb509c4bfbc0
Muhammad Sohaib <123618118+Sohaib110@users.noreply.github.com>
2024-12-23 01:25:19 +0500

    third commit

--- app/models.py
+++ app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class GeneralInfo(models.Model):
+    company_name = models.CharField(max_length=250, default='Company')
+    location = models.CharField(max_length=250)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    open_hours = models.CharField(max_length=100, blank=True, null=True)
+    video_url = models.URLField(blank=True, null=True)
+    twitter = models.URLField(blank=True, null=True)
+    facebook = models.URLField(blank=True, null=True)
+    instagram = models.URLField(blank=True, null=True)
+    linkedin = models.URLField(blank=True, null=True)
+    
+    
+    
+    
