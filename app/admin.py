26d8df7c6c6eccd121d365f322a90435c188c2d6
Muhammad Sohaib <123618118+Sohaib110@users.noreply.github.com>
2024-12-23 01:43:19 +0500

    fourth commit

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from app.models import GeneralInfo
 
-# Register your models here.
+@admin.register(GeneralInfo)
+class GeneralInfoAdmin(admin.ModelAdmin):
+   list_display = ['company_name', 'location', 'email', 'phone', 'open_hours']
+   
+   read_only_fields = ['email']
+   
+    
