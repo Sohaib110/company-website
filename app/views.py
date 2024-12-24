fffff070d3fc0be8dacc3e4baf829f6a65f35135
Muhammad Sohaib <123618118+Sohaib110@users.noreply.github.com>
2024-12-24 22:23:38 +0500

    fifth

--- app/views.py
+++ app/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from app.models import GeneralInfo
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html', {}) 
+    
+    
+    
+    
+    all_records = GeneralInfo.objects.all()
+    print(all_records)
+    
+    
+    
+    
+    
+    
+    
+    context={}
+    return render(request, 'index.html', context) 
     
