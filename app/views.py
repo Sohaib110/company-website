5c9fc3a799bea88dec8c4328eb16be557ccdd200
Muhammad Sohaib <123618118+Sohaib110@users.noreply.github.com>
2024-12-27 22:26:53 +0500

    seventh

--- app/views.py
+++ app/views.py
@@ -1,31 +1,9 @@
 from django.shortcuts import render
 from app.models import GeneralInfo
-
-from django.db import connection
-def write_sql_queries_to_file(file_path):
-    with open(file_path, 'w') as file:
-        queries = connection.queries
-        for query in queries:
-            sql= query['sql']
-            file.write(f"{sql}\n")
-
-
 # Create your views here.
 def index(request):
     
-    
-    
-    
-    #all_records = GeneralInfo.objects.all()
-    #print(all_records)
-    
-    
-    
-    
-    
-    
-    file_path = 'sql_queries.txt'
-    write_sql_queries_to_file(file_path)
+
     context={}
     return render(request, 'index.html', context) 
     
