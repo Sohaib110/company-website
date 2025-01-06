13f183cb8a83e58e9dfbbeaf119d796d1656bece
Muhammad Sohaib <123618118+Sohaib110@users.noreply.github.com>
2025-01-06 22:54:22 +0500

    contact

--- app/admin.py
+++ app/admin.py
@@ -41,13 +41,10 @@ class ContactFormLogAdmin(admin.ModelAdmin):
       'action_time'
       
    ]
-   
    def has_add_permission(self, request, obj=None):
       return False
-   
    def has_change_permission(self, request, obj = None):
       return False
-   
    def has_delete_permission(self, request, obj = None):
       return False 
        
